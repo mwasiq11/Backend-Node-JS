587fdb287208a8f35d5275a1289a3736b79a689e
mwasiq11 <muhammadwasiq67585@gmail.com>
2025-09-17 02:48:16 +0500

    implementing Express routing

--- Leacture 6-Express JS/app.js
+++ Leacture 6-Express JS/app.js
@@ -1,16 +1,25 @@
-const express=require('express')
-const app=express()
+const express = require("express");
+const app = express();
 // middleware to connect app with server
-app.use((req,res,next)=>{
-	console.log("first middleware",req.url,req.method);
-	res.send("<p>Learning Express JS</p>")
-	next();
-})
-app.use((req,res,next)=>{
-	console.log("second middleware",req.url,req.method);
-	next();
-})
-const PORT=3000;
-app.listen(PORT,()=>{
-	console.log(`server running http://localhost:${PORT}`)
-})
+// Global middleware (runs for every request)
+app.use((req, res, next) => {
+  console.log("Global middleware:", req.url, req.method);
+  next();
+});
+
+// Home route
+app.get("/", (req, res) => {
+  console.log("Home route hit:", req.url, req.method);
+  res.send("<h1>Express JS</h1>");
+});
+
+// Submit-details route
+app.get("/submit-details", (req, res) => {
+  console.log("Submit-details route hit:", req.url, req.method);
+  res.send("<p>Learning Express JS</p>");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
