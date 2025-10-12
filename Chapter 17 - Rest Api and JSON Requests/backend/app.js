9d667f7fb3ca95a514e96b66d24aa0b4844cfd49
mwasiq11 <muhammadwasiq67585@gmail.com>
2025-10-13 01:15:32 +0500

    feat: customizing UI and adding REST API functionality

--- Chapter 17 - Rest Api and JSON Requests/backend/app.js
+++ Chapter 17 - Rest Api and JSON Requests/backend/app.js
@@ -3,32 +3,25 @@ const path = require('path');
 
 // External Module
 const express = require('express');
-const session=require('express-session');
-const errorsController=require('./controllers/errors')
-const MongoDBStore = require('connect-mongodb-session')(session);
-const DB_PATH="mongodb+srv://root:root00001@node.y1gwx8t.mongodb.net/?retryWrites=true&w=majority&appName=Node"
+const { default: mongoose } = require('mongoose');
+const cors = require('cors');
+const DB_PATH="mongodb+srv://root:root00001@node.y1gwx8t.mongodb.net/?retryWrites=true&w=majority&appName=Node";
 
 //Local Module
-const rootDir = require("./utils/pathUtil");
-const { default: mongoose } = require('mongoose');
+const todoItemsRouter = require("./routes/todoItemsRouter")
+const errorsController = require("./controllers/errors");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-
-const store = new MongoDBStore({
-  uri: DB_PATH,
-  collection: 'sessions'
-});
-app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(cors());
 
+app.use("/api/todo", todoItemsRouter);
 
-app.use(express.static(path.join(rootDir, 'public')));
 app.use(errorsController.pageNotFound);
 
-const PORT = 3003;
+const PORT = 3001;
 
 mongoose.connect(DB_PATH).then(() => {
   console.log('Connected to Mongo');
